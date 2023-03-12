842052e2f9b5f10b09922d17946c4d21da497ba4
Guilherme Pessoa <gui.senai@live.com>
2023-03-12 07:56:43 -0300

    User insert

--- src/main/java/com/educandoweb/course/resourses/OrderResource.java
+++ src/main/java/com/educandoweb/course/resourses/OrderResource.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.OrderService;
 
 @RestController
@@ -30,4 +32,5 @@ public class OrderResource {
 		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
 }
